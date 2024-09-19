2a73afd48f92517c786a248e9af8ce236596cf3e
pekoniko <45590625+Masterlolan@users.noreply.github.com>
2024-09-19 22:47:17 +0300

    sql and maven issues

--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -1,7 +1,7 @@
 create SCHEMA IF NOT EXISTS Employees;
 create TABLE IF NOT EXISTS employee
 (
-    ID  integer PRIMARY KEY,
+    id  bigserial PRIMARY KEY,
     first_name  VARCHAR(100) NOT NULL,
     last_name  VARCHAR(100) NOT NULL,
     position VARCHAR(100) NOT NULL,
@@ -11,14 +11,14 @@ create TABLE IF NOT EXISTS salary
 (
     ID  integer PRIMARY KEY,
     employee_id  integer NOT NULL UNIQUE,
-    amount  real NOT NULL,
+    amount  decimal(12,4)  NOT NULL,
     start_date DATE  NOT NULL
 );
 create TABLE IF NOT EXISTS salary_history
 (
     ID  integer PRIMARY KEY,
     employee_id  integer NOT NULL,
-    amount  real NOT NULL,
+    amount  decimal(12,4)  NOT NULL,
     start_date DATE  NOT NULL,
     end_date DATE
 );
